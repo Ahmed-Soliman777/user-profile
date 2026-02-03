4c950ac925d6a5acc6568074500a7cb5a4ae64b1
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-02-03 21:12:51 +0200

    Create zod schema

---
+++ src/app/utils/schema.ts
@@ -0,0 +1,53 @@
+import { z } from "zod";
+
+const passwordRegex = new RegExp(
+  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/,
+);
+
+export const CreateUserSchema = z.object({
+  FirstName: z
+    .string({ error: "First name is required" })
+    .min(2, { error: "First name have to be at least 2 chars" }),
+
+  LastName: z
+    .string({ error: "Last name is required" })
+    .min(2, { error: "Last name have to be at least 2 chars" }),
+
+  Email: z.email({ error: "Invalid mail" }),
+
+  Password: z.string({ error: "Invalid Password" }).regex(passwordRegex, {
+    message:
+      "Password should be at least 8 length & includes a char, a symbol and a number",
+  }),
+
+  Image: z.string().optional(),
+});
+
+export const LoginUserSchema = z.object({
+  Email: z.email({ error: "Invalid credentials!" }),
+  Password: z.string({ error: "Invalid credentials!" }),
+});
+
+export const UpdateUserSchema = z.object({
+  FirstName: z
+    .string({ error: "First name is required" })
+    .min(2, { error: "First name have to be at least 2 chars" })
+    .optional(),
+
+  LastName: z
+    .string({ error: "Last name is required" })
+    .min(2, { error: "Last name have to be at least 2 chars" })
+    .optional(),
+
+  Email: z.email({ error: "Invalid mail" }).optional(),
+
+  Password: z
+    .string({ error: "Invalid Password" })
+    .regex(passwordRegex, {
+      message:
+        "Password should be at least 8 length & includes a char, a symbol and a number",
+    })
+    .optional(),
+
+  Image: z.string().optional(),
+});
