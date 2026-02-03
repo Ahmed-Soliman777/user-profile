e40ade2b4cb4ed7f9816aa254125a12250d20153
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-02-03 21:13:19 +0200

    Create verify token file

---
+++ src/app/utils/verifyToken.ts
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import { JWTPayload } from "./types";
+import { NextRequest } from "next/server";
+
+export function VerifyToken(request: NextRequest): JWTPayload | null {
+  try {
+    const JwtToken = request.cookies.get("token");
+
+    const token = JwtToken?.value as string;
+
+    if (!token) return null;
+
+    const UserPayload = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string,
+    ) as JWTPayload;
+
+    return UserPayload;
+  } catch (error) {
+    return null;
+  }
+}
